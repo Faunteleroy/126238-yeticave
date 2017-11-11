36aa5b55b5dd7cd2d47b438d40741ca4aa8df57c
Faunteleroy <faunteler@gmail.com>
2017-11-11 03:13:32 +0300

    Добавила константы и изменила функции

--- index.php
+++ index.php
@@ -16,15 +16,13 @@ $tomorrow = strtotime('tomorrow midnight');
 // временная метка для настоящего времени
 $now = strtotime('now');
 
-$count_hour = floor(($tomorrow - $now)/3600);
-if ($count_hour < 10) {
-    $count_hour = 0 . $count_hour;
-}
-$count_min = floor(($tomorrow - $now)/60)%60;
-if ($count_min < 10) {
-    $count_min = 0 . $count_min;
-}
+define("SECONDS_IN_HOUR", 3600);
+define("SECONDS_IN_MIN", 60);
 
+$count_hour = floor(($tomorrow - $now) / SECONDS_IN_HOUR);
+str_pad($count_hour, 2, "0", STR_PAD_LEFT);
+$count_min = floor((($tomorrow - $now) % SECONDS_IN_HOUR) / SECONDS_IN_MIN);
+str_pad($count_min, 2, "0", STR_PAD_LEFT);
 $lot_time_remaining = "$count_hour" . ":" . "$count_min";
 // далее нужно вычислить оставшееся время до начала следующих суток и записать его в переменную $lot_time_remaining
 ?>
